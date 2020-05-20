7fb3ae24aa1cf4de9d3c818e4aa5f392e5898b19
Justin C Williams <45780143+justinwilly@users.noreply.github.com>
2020-05-20 13:13:16 -0400

    mvp for day 1 and day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,33 +2,72 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+MUL = 0b10100010
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0
+        self.reg = [0] * 8
+        self.ram = [0] * 256
 
-    def load(self):
+# Inside the CPU, there are two internal registers used for memory operations: the Memory Address Register (MAR) and the Memory Data Register (MDR). The MAR contains the address that is being read or written to. The MDR contains the data that was read or the data to write. You don't need to add the MAR or MDR to your CPU class, but they would make handy paramter names for ram_read() and ram_write(), if you wanted.
+
+    def ram_read(self, mar):
+        mdr = self.ram[mar]
+        return mdr
+
+    def ram_write(self, mdr, mar):
+        self.ram[mar] = mdr
+
+
+
+    def load(self, filename):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+        try:
+            address = 0
+            #open the file
+            with open(sys.argv[1]) as f:
+                #read every line
+                for line in f:
+                    #parse out comments
+                    comment_split = line.strip().split("#")
+                    # Cast number string to int
+                    value = comment_split[0].strip()
+                    #ignore blank lines
+                    if value == "":
+                        continue
+                    instruction = int(value, 2)
+                    #populate memory array
+                    self.ram[address] = instruction
+                    address += 1
+
+        except:
+            print("cant find file")
+            sys.exit(2)
 
 
     def alu(self, op, reg_a, reg_b):
@@ -37,6 +76,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,4 +103,23 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while True:
+            opcode = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            if opcode == LDI:
+                # set the value of a register to an integer
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif opcode == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif opcode == MUL:
+                self.alu(opcode, operand_a, operand_b)
+                self.pc += 3
+            elif opcode == HLT:
+                # exiting the system if HLT is encountered
+                sys.exit(0)
+            else:
+                print(f"Did not work")
+                sys.exit(1)
---
+++ pract.py
@@ -0,0 +1,25 @@
+# Given an object/dictionary with keys and values that consist of both strings and integers, design an algorithm to calculate and return the sum of all of the numeric values.
+# For example, given the following object/dictionary as input:
+# {
+#   "cat": "bob",
+#   "dog": 23,
+#   19: 18,
+#   90: "fish"
+# }
+# Your algorithm should return 41, the sum of the values 23 and 18.
+# You may use whatever programming language you'd like.
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+
+d = {"cat": "bob",
+    "dog": 23,
+    19: 18,
+    90: "fish"
+    }
+sum = 0
+for i in d.values():
+    if type(i) == int:
+        sum += i
+print(sum)
+
+#.values to get values out of a list
+#.key to get the key out of the list
